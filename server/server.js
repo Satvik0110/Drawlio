5b5f2cc8491a077624255d3513f1fab18f7a1523
Satvik <satvikgarg200@gmail.com>
2025-07-23 16:45:36 +0530

    added server port env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
 const cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
